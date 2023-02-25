8b40e044bc7daade329e5bb84f8606f453f5a66b
orlova.julia.ivan@gmail.com <orlova.julia.ivan@gmail.com>
2023-02-25 23:54:41 +0300

    Добавьте файлы проекта.

---
+++ 23 Task_async/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _23_Task_async
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=Convert.ToInt32(Console.ReadLine());
+            Sum(n);
+           Console.ReadKey();
+        }
+
+        static void Sum(int n)
+        {
+            int s = 1;
+            for (int i = 0; i < n; i++)
+            {
+                s *= i;
+                Thread.Sleep(10);  
+            }
+            Console.WriteLine(s);
+            
+        }
+
+        static async void SumAsync(int n)
+        {
+            await Task.Run(()=>Sum(n));
+        }
+
+        //Console.ReadKey();
+    }
+}
